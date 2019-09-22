e7c99d6ed74205633755972bff47440ccb23c0fb
Ermondel <ermondel@gmail.com>
2019-09-22 20:14:46 +0300

    feat: define class for user data

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.log('start');
+import { User } from './models/User';
+
+const user = new User({ name: 'myname', age: 1 });
+
+user.set({ name: 'newname' });
+
+console.log(user.get('name'));
+console.log(user.get('age'));
