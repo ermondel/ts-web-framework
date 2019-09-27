829f57036e7404b4882fbc72f51cbb5b8f68dbd2
Ermondel <ermondel@gmail.com>
2019-09-27 16:01:23 +0300

    feat: saving user data

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,20 @@
 import { User } from './models/User';
 
-const user = new User({ name: 'myname', age: 1 });
+// const user = new User({ id: 1 });
+// user.set({ name: 'NEW NAME', age: 99 });
+// user.save();
 
-user.on('change', () => {
-  console.log('Change #1');
-});
-user.on('change', () => {
-  console.log('Change #2');
-});
-user.on('save', () => {
-  console.log('Save #1');
-});
+const user = new User({ name: 'new record', age: 0 });
 
-user.trigger('save');
+user.save();
+
+// user.on('change', () => {
+//   console.log('Change #1');
+// });
+// user.on('change', () => {
+//   console.log('Change #2');
+// });
+// user.on('save', () => {
+//   console.log('Save #1');
+// });
+// user.trigger('save');
