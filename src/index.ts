77f256441bc4b28c6840aabfa713f5fccea35db3
Ermondel <ermondel@gmail.com>
2019-10-05 20:04:38 +0300

    refactor: eventing system with composition

--- src/index.ts
+++ src/index.ts
@@ -6,15 +6,15 @@ import { User } from './models/User';
 
 const user = new User({ name: 'new record', age: 0 });
 
-user.save();
+// user.save();
 
-// user.on('change', () => {
-//   console.log('Change #1');
-// });
-// user.on('change', () => {
-//   console.log('Change #2');
-// });
+user.events.on('change', () => {
+  console.log('Change #1');
+});
+user.events.on('change', () => {
+  console.log('Change #2');
+});
 // user.on('save', () => {
 //   console.log('Save #1');
 // });
-// user.trigger('save');
+user.events.trigger('change');
