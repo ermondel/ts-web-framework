820096c9d6c0338150299f0723751c6729ae0991
Ermondel <ermondel@gmail.com>
2019-10-09 12:53:39 +0300

    feat: provide fetching and saving user data

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,9 @@
 import { User } from './models/User';
 
-// const user = new User({ id: 1 });
-// user.set({ name: 'NEW NAME', age: 99 });
-// user.save();
+const user = new User({ id: 1, name: 'new name', age: 0 });
 
-const user = new User({ name: 'new record', age: 0 });
-
-console.log(user.get('name'));
-
-user.on('change', () => {
-  console.log('Change #1');
+user.on('save', () => {
+  console.log(user);
 });
-user.on('change', () => {
-  console.log('Change #2');
-});
-// user.on('save', () => {
-//   console.log('Save #1');
-// });
-user.events.trigger('change');
+
+user.save();
